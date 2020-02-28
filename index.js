60a2b1e75e4ddd9c8b53c05dda942332eadfc3a0
Md.Abdus Salam <discovercoachingcenter2018@gmail.com>
2020-02-28 21:27:05 +0600

    first second time commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var jobpaiso = true;
+var saving = 20000;
+
+if(jobpaiso == true && saving>200000){
+    console.log("cholo tomar jonno bow khuji");
+}
+else{
+    console.log("tomake biye korte hobena ..........jhonkar mahbub vai boleche");
+}
